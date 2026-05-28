f831e87bdd4ebea310673c8d911ad0667d6c4fc3
GIlbertTjandra <starjdoy@gmail/com>
2026-05-28 20:53:11 +0700

    fix port for railway

--- backend/server.py
+++ backend/server.py
@@ -150,4 +150,6 @@ def serve(path):
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
